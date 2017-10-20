0955b2233d2c1080afa0a72f4283e582645573f7
thekelvinliu <kelvin@thekelvinliu.com>
2017-10-19 23:26:36 -0700

    added more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: 'airbnb-base',
   rules: {
+    'arrow-parens': ['error', 'as-needed'],
     'comma-dangle': ['error', 'never'],
+    curly: ['error', 'multi-or-nest'],
     'valid-jsdoc': [
       'error',
       {
