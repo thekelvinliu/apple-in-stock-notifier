57393da0b53a4ed1ece5170fbe45497918032ccc
thekelvinliu <kelvin@thekelvinliu.com>
2017-10-19 21:42:14 -0700

    added serverless stuff

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, 'lib'),
+    filename: '[name].js'
+  },
+  externals: ['aws-sdk'],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
