78d655192f0f0ac3920a19663388bd68b708dbbd
thekelvinliu <kelvin@thekelvinliu.com>
2017-10-19 23:29:33 -0700

    wrote code to make the request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import { SNS } from 'aws-sdk';
+// import { SNS } from 'aws-sdk';
 import fetch from 'node-fetch';
+import { URL } from 'url';
+
+const BASE_URL = 'https://www.apple.com/shop/retail/pickup-message';
 
 /**
  * requests apple product availability for a given part number and location
@@ -9,6 +12,37 @@ import fetch from 'node-fetch';
  * @returns {Object} the result of the callback function
  */
 export default function handler(event, context, callback) {
-  console.log(fetch, SNS);
-  return callback(null, 'ok');
+  // log incoming event
+  console.log('received event');
+  console.log(JSON.stringify(event));
+
+  // validate variables
+  const { SNS_ARN: TopicArn } = process.env;
+  if (!TopicArn)
+    return callback(new Error('the SNS_TOPIC environment variable must be set'));
+  const {
+    cppart,
+    partNumber,
+    zipcode
+  } = event;
+  if (!partNumber)
+    return callback(new Error('the event object is missing its partNumber property'));
+  if (!zipcode)
+    return callback(new Error('the event object is missint its zipcode property'));
+
+  // construct url
+  const url = new URL(BASE_URL);
+  url.searchParams.set('location', zipcode);
+  url.searchParams.set('parts.0', partNumber);
+  if (cppart)
+    url.searchParams.set('cppart', cppart);
+
+  // make request
+  return fetch(url.toString())
+    .then(res => res.json())
+    .then(json => {
+      console.log(JSON.stringify(json));
+      return callback(null, 'ok');
+    })
+    .catch(err => callback(err));
 }
