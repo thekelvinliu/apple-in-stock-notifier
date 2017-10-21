ce01de5652b2b1747c646c67d5c453d0f2b569d5
thekelvinliu <kelvin@thekelvinliu.com>
2017-10-21 16:20:20 -0700

    finished lambda implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-// import { SNS } from 'aws-sdk';
+import { SNS } from 'aws-sdk';
 import fetch from 'node-fetch';
 import { URL } from 'url';
 
 const BASE_URL = 'https://www.apple.com/shop/retail/pickup-message';
+
+const cache = new Map();
+const sns = new SNS();
 
 /**
  * requests apple product availability for a given part number and location
@@ -11,7 +14,7 @@ const BASE_URL = 'https://www.apple.com/shop/retail/pickup-message';
  * @param {Function} callback - the lambda callback function
  * @returns {Object} the result of the callback function
  */
-export default function handler(event, context, callback) {
+export default async function handler(event, context, callback) {
   // log incoming event
   console.log('received event');
   console.log(JSON.stringify(event));
@@ -37,12 +40,77 @@ export default function handler(event, context, callback) {
   if (cppart)
     url.searchParams.set('cppart', cppart);
 
-  // make request
-  return fetch(url.toString())
-    .then(res => res.json())
-    .then(json => {
-      console.log(JSON.stringify(json));
-      return callback(null, 'ok');
-    })
-    .catch(err => callback(err));
+  try {
+    // get json response
+    const json = await fetch(url.toString()).then(res => res.json());
+    // grab store data
+    const { body: { stores } } = json;
+    if (!stores || !Array.isArray(stores))
+      throw new Error('body.stores in the json response is not an array');
+    // format data
+    const data = stores
+      .filter(obj => obj.storedistance <= 15)
+      .map(obj => {
+        const {
+          storeEmail: email,
+          reservationUrl: homepage,
+          storeNumber: id,
+          storeName: name,
+          phoneNumber,
+          partsAvailability: {
+            [event.partNumber]: {
+              storePickupQuote: message,
+              storePickupProductTitle: product,
+              pickupDisplay: status
+            }
+          }
+        } = obj;
+        return {
+          id,
+          name,
+          product,
+          available: status === 'available',
+          message,
+          email,
+          phone: phoneNumber.replace(/\D/g, ''),
+          homepage
+        };
+      });
+    // cache new data
+    const baseKey = Object.keys(event).sort().map(k => event[k]).join('|');
+    await Promise.all(data.map(async incoming => {
+      const key = `${baseKey}|${incoming.id}`;
+      const current = cache.get(key);
+      // do nothing if there's no change
+      if (current && current.available === incoming.available) {
+        console.log(`no change for key: '${key}'`);
+        return;
+      }
+      // save the incoming data
+      cache.set(key, incoming);
+      // create message
+      const msg = [
+        incoming.product,
+        'is now',
+        incoming.available ? 'available' : 'unavailable',
+        'at',
+        incoming.name
+      ].join(' ');
+      const payload = {
+        Subject: msg.toLowerCase(),
+        Message: JSON.stringify(incoming).toLowerCase(),
+        TopicArn
+      };
+      console.log(`publishing payload for key: '${key}'`);
+      console.log(JSON.stringify(payload));
+      // publish payload to sns
+      const { MessageId } = await sns.publish(payload).promise();
+      console.log(`successfully published message with id '${MessageId}'`);
+    }));
+  } catch (err) {
+    console.error(err);
+    return callback(err);
+  }
+
+  return callback(null, 'ok');
 }
